31af3772328d3bfea316269dcea5d7a47fe520e3
junhi <ghkd2007@gmail.com>
2025-08-11 00:13:16 +0900

    Fix index.ts for Expo web deployment - use registerRootComponent

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import App from "./App";
 
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// Expo 앱의 루트 컴포넌트로 App을 등록
+// 이는 Expo Go와 네이티브 빌드 모두에서 적절한 환경을 설정합니다
 registerRootComponent(App);
